c52680a652147ffdb1d19ba22957b07a95b6df94
Renanfls <contato.renanfabricio@gmail.com>
2024-03-10 01:25:59 -0300

    Add SectionVideo

---
+++ src/components/SectionVideo/index.js
@@ -0,0 +1,21 @@
+export default function SectionVideo() {
+  return (
+    <section className="container-fluid sectionVideo p-4 py-5 p-md-5 d-flex flex-column flex-lg-row justify-content-between align-items-center gap-5">
+      <iframe
+        className="sectionVideo__video mx-3 rounded-4 bg-white"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+      <div className="sectionVideo__text">
+        <h1 className="fw-bold">Lorem Ipsum is simply dummy text of the...</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum is simply dummy text of the printing and
+          typesetting.
+        </p>
+      </div>
+    </section>
+  );
+}
